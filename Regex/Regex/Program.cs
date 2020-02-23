3efd9e78bb6014abbbd09c209c336977ee16db2a
Balázs Tóth <tothbalazs@live.com>
2020-02-23 12:47:26 +0100

    Initialize Regex solution

---
+++ Regex/Regex/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string pattern = "^[a-z]+@[a-z]+.[a-z]+$"; Email
+            string pattern = @"^\+36(20|30|70){1}[0-9]{7}$";
+            Regex r = new Regex(pattern);
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
