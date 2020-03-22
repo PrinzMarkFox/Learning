696e2e4fca6672ec1e1b1c95d4c18de6390a2b74
Balázs Tóth <tothbalazs@live.com>
2020-03-22 10:58:04 +0100

    Egyszerűbb megoldás

---
+++ Gyakorlás/Practice/Practice/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(ToUpper2(" aaaaezaegyatesztasZöVeGaaag"));
+            Console.ReadKey();
+        }
+
+        private static bool isSeparator(char character)
+        {
+            return (new HashSet<char>() { ' ', 'a', ',' }).Contains(character);
+        }
+
+        private static string toUpperCase(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new ArgumentNullException();
+            }
+
+            string result = "";
+            bool toUpper = true;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (toUpper && !isSeparator(text[i]))
+                {
+                    result += char.ToUpper(text[i]);
+                    toUpper = false;
+                }
+                else
+                {
+                    if (isSeparator(text[i]))
+                    {
+                        result += text[i];
+                        toUpper = true;
+                    }
+                    else
+                    {
+                        result += text[i];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        static string ToUpper2(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                throw new ArgumentNullException();
+
+            string result = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (i == 0 && !isSeparator(text[i]) || i > 0 && isSeparator(text[i - 1]) && !isSeparator(text[i]))
+                    result += char.ToUpper(text[i]);
+                else
+                    result += text[i];
+            }
+
+            return result;
+        }
+    }
+}
